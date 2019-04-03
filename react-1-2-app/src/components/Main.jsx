e20c2444b6afe3dbf2c40e9d5bd85cde2040f305
Brian Gardiner <bgardiner9001@gmail.com>
2019-04-03 17:01:07 -0600

    basic function and style complete, black diamond incomplete

--- react-1-2-app/src/components/Main.jsx
+++ react-1-2-app/src/components/Main.jsx
@@ -7,13 +7,17 @@ export default class Main extends Component{
                 <div className = 'main-box'>
                     <h1 id = 'counter'>{this.props.index + 1}/25</h1>
                     <h1 id = 'name'>{this.props.data[this.props.index].name.first} {this.props.data[this.props.index].name.last}</h1>
-                    <h4>From: {this.props.data[this.props.index].city}, {this.props.data[this.props.index].country}</h4>
-                    <h4>Job Title: <span>{this.props.data[this.props.index].title}</span></h4>
-                    <h4>Employer: {this.props.data[this.props.index].employer}</h4>
-                    <h4>Favorite Movies:</h4>
-                    <p className = 'movie-list'>1: {this.props.data[this.props.index].favoriteMovies[0]}</p>
-                    <p className = 'movie-list'>2: {this.props.data[this.props.index].favoriteMovies[1]}</p>
-                    <p className = 'movie-list'>3: {this.props.data[this.props.index].favoriteMovies[2]}</p>
+                    <span>
+                        <p><span className= 'bold'>From: </span>{this.props.data[this.props.index].city}, {this.props.data[this.props.index].country}</p>
+                        <p><span className= 'bold'>Job Title: </span>{this.props.data[this.props.index].title}</p>
+                        <p><span className= 'bold'>Employer: </span>{this.props.data[this.props.index].employer}</p>
+                    </span>
+                    <h4 id = 'favorites'>Favorite Movies:</h4>
+                    <span className = 'movie-list'>
+                        <p className = 'movie-list'>1: {this.props.data[this.props.index].favoriteMovies[0]}</p>
+                        <p className = 'movie-list'>2: {this.props.data[this.props.index].favoriteMovies[1]}</p>
+                        <p className = 'movie-list'>3: {this.props.data[this.props.index].favoriteMovies[2]}</p>
+                    </span>
 
                 </div>
             </main>
